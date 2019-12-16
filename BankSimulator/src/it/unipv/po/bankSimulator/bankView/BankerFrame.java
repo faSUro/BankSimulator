027de64fe1ffd05364d998639b2a268cbefe40e8
fasuro <fasuro@faSUro>
2019-12-16 19:49:50 +0100

    added a JComboBox to select what type of account to create (BankerFrame)

--- BankSimulator/src/it/unipv/po/bankSimulator/bankView/BankerFrame.java
+++ BankSimulator/src/it/unipv/po/bankSimulator/bankView/BankerFrame.java
@@ -4,16 +4,19 @@ import java.awt.Container;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import it.unipv.po.bankmodel.account.AccountType;
+
 @SuppressWarnings("serial")
 public class BankerFrame extends JFrame {
 	
 	public static final int WIDTH = 600;
-	public static final int HEIGHT = 450;
+	public static final int HEIGHT = 525;
 	
 	public BankerFrame(String name) {
 		setTitle(name);
@@ -36,22 +39,30 @@ public class BankerFrame extends JFrame {
 		                                                                        //check the status of bank accounts
 		checkPanel.add(checkButton);
 		
+		JLabel fiscalCodeLabel = new JLabel("     Fiscal code: ");
 		JTextField addAccountTextField = new JTextField(20); //insert here the fiscal code to create a new account
+		String[] accountType = {"Bank account", "Deposit account", "Web account"};
+		JComboBox<String> accountTypeSelection = new JComboBox<String>(accountType); //types of account that can be created
 		JButton addAccountButton = new JButton("Add account"); //this button add a new account
+		addAccountPanel.add(fiscalCodeLabel);
 		addAccountPanel.add(addAccountTextField);
+		addAccountPanel.add(accountTypeSelection);
 		addAccountPanel.add(addAccountButton);
 		
-		JLabel ibanLabel = new JLabel("Insert iban here:    "); 
+		JLabel emptyLabel1 = new JLabel("                                                                     "
+				+ "                                               ");
+		JLabel ibanLabel = new JLabel("                   IBAN:    "); 
 		JTextField ibanTextField = new JTextField(20); //insert here the iban of the account
-		JLabel amountLabel = new JLabel("                        Insert amount here: ");
+		JLabel amountLabel = new JLabel("                                           Amount: ");
 		JTextField amountTextField = new JTextField(20); //insert here the amount to add to/subtract from the account
-		JLabel emptyLabel = new JLabel("                    ");
+		JLabel emptyLabel2 = new JLabel("                    ");
 		JButton okButton = new JButton("  Ok  "); //this button performs the operation
+		operationPanel.add(emptyLabel1);
 		operationPanel.add(ibanLabel);
 		operationPanel.add(ibanTextField);
 		operationPanel.add(amountLabel);
 		operationPanel.add(amountTextField);
-		operationPanel.add(emptyLabel);
+		operationPanel.add(emptyLabel2);
 		operationPanel.add(okButton);
 		
 		panel.add(emptyPanel1);
